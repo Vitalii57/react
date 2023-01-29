7b277a49a2e3f145a11062cf71fb3ea1d51b8dd2
Vitalii57 <vitalij.zubkov.2017@inbox.ru>
2023-01-29 21:59:36 +0300

    2

--- test_22/src/components/ShopItemFunc.js
+++ test_22/src/components/ShopItemFunc.js
@@ -1,21 +1,25 @@
-function ShopItemFunc(props) {
+import PropTypes from "prop-types";
+
+export default function ShopItemFunc({ item }) {
   return (
     <div className="main-content">
-      <h2>{props.item.brand}</h2>
-      <h1>{props.item.title}</h1>
-      <h3>{props.item.description}</h3>
-      <div className="description">{props.item.descriptionFull}</div>
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
       <div className="highlight-window mobile">
         <div className="highlight-overlay"></div>
       </div>
       <div className="divider"></div>
       <div className="purchase-info">
         <div className="price">
-          {props.item.currency} {props.item.price.toFixed(2)}
+          {item.currency} {item.price.toFixed(2)}
         </div>
         <button>Добавить в корзину</button>
       </div>
     </div>
   );
 }
-export default ShopItemFunc;
+ShopItemFunc.PropTypes = {
+  item: PropTypes.objectOf(PropTypes.string.isRequired),
+};
